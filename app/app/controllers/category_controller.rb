47040405923f3b1936e497db20b9bda5a954a5d3
wangxiaobo <boming1989@Gmail.com>
2022-01-03 10:35:01 +0800

    修改git 不用上传文件及删除一些不用gem

--- app/app/controllers/category_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class CategoryController < ApplicationController
-  def index
-    categories = Category.all
-    render status: :ok, json: { categories: categories }
-  end
-end
--- app/test/controllers/category_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-
-class CategoryControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
