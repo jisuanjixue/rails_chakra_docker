ee6c52fd56694fdf19e8047d4259db0bacbcaf79
bobo <bomimg1989@gmail.com>
2022-03-27 15:25:20 +0800

    上传头像修复

--- app/app/javascript/components/upload/FileUpload.tsx
+++ app/app/javascript/components/upload/FileUpload.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback } from "react";
-import { FormLabel, FormControl, Button, Container, Box, Input, Text } from "@chakra-ui/react";
+import { FormLabel, FormControl, Text, VisuallyHidden, Stack, Flex, chakra, Icon, useColorModeValue } from "@chakra-ui/react";
 import { useDropzone } from "react-dropzone";
-import Thumb from "./Thumb";
 import uploadApi from "../../apis/upload";
 
 const FileUpload = ({ formik }) => {
@@ -29,21 +28,46 @@ const FileUpload = ({ formik }) => {
   };
 
   return (
-    <Box as="form">
-      <FormControl>
-        <FormLabel>头像</FormLabel>
-        <Container w={100} h={200}>
-          <Box {...getRootProps({ className: "dropzone" })}>
-            <Input name="file" {...getInputProps()} />
-            <Text>拖动文件到此或点击上传</Text>
-          </Box>
-          <Thumb files={acceptedFiles} />
-        </Container>
-      </FormControl>
-      <Button loadingText="上传中" onClick={() => handUpload()} colorScheme="teal" variant="outline">
-        上传
-      </Button>
-    </Box>
+    <FormControl>
+      <FormLabel fontSize="sm" fontWeight="md" color={useColorModeValue("gray.700", "gray.50")}>
+        头像
+      </FormLabel>
+      <Flex mt={1} justify="center" px={6} pt={5} pb={6} borderWidth={2} borderColor={useColorModeValue("gray.300", "gray.500")} borderStyle="dashed" rounded="md">
+        <Stack spacing={1} textAlign="center">
+          <Icon mx="auto" boxSize={12} color={useColorModeValue("gray.400", "gray.500")} stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+            <path
+              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </Icon>
+          <Flex fontSize="sm" color={useColorModeValue("gray.600", "gray.400")} alignItems="baseline">
+            <chakra.label
+              htmlFor="file-upload"
+              cursor="pointer"
+              rounded="md"
+              fontSize="md"
+              onClick={() => handUpload()}
+              color={useColorModeValue("brand.600", "brand.200")}
+              pos="relative"
+              _hover={{
+                color: useColorModeValue("brand.400", "brand.300"),
+              }}
+            >
+              <span>上传头像</span>
+              <VisuallyHidden {...getRootProps({ className: "dropzone" })}>
+                <input id="file-upload" name="file-upload" type="file" {...getInputProps()} />
+              </VisuallyHidden>
+            </chakra.label>
+            <Text pl={1}>拖动图片到此或点击上传</Text>
+          </Flex>
+          <Text fontSize="xs" color={useColorModeValue("gray.500", "gray.50")}>
+            PNG, JPG, GIF up to 10MB
+          </Text>
+        </Stack>
+      </Flex>
+    </FormControl>
   );
 };
 
